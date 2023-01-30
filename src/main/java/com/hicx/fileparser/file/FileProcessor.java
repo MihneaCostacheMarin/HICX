1ecbead0e55f9d46e47658db7db75a08df78a270
Mihnea <mihneacostachemarin@gmail.com>
2023-01-30 16:16:09 +0200

    File statistics class
    Create the FileStatistics class that takes care of storing the number of dots, words and most frequent word.

--- src/main/java/com/hicx/fileparser/file/FileProcessor.java
+++ src/main/java/com/hicx/fileparser/file/FileProcessor.java
@@ -1,4 +1,17 @@
 package com.hicx.fileparser.file;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileProcessor {
+    private File file;
+
+    public FileProcessor(File file) {
+        this.file = file;
+    }
+
+    public void processFile() {
+
+    }
 }
